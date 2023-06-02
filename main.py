052fa5540cceb03da65884b7157e312caed04b86
José Rodríguez <jose.rodriguezsosa@outlook.com>
2023-06-01 21:36:23 -0500

    data validation pipeline created

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from textSummarizer.logging import logger
+from textSummarizer.pipeline.stage_02_data_validation import DataValidationPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 try:
     logger.info(f">>>> stage {STAGE_NAME} started <<<<")
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.main()
+    logger.info(f">>>> stage {STAGE_NAME} completed <<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Data Validation Stage"
+try:
+    logger.info(f">>>> stage {STAGE_NAME} started <<<<")
+    data_validation = DataValidationPipeline()
+    data_validation.main()
     logger.info(f">>>> stage {STAGE_NAME} completed <<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
