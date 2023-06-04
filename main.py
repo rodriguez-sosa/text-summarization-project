2eeef36a7373db212530d0d05ba9ea8570898ef1
José Rodríguez <jose.rodriguezsosa@outlook.com>
2023-06-04 13:53:31 -0500

    trainer and evalution pipelines created

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from textSummarizer.logging import logger
 from textSummarizer.pipeline.stage_02_data_validation import DataValidationPipeline
+from textSummarizer.pipeline.stage_03_data_transformation import DataTransformationPipeline
+from textSummarizer.pipeline.stage_04_model_trainer import ModelTrainerPipeline
+from textSummarizer.pipeline.stage_05_model_evaluation import ModelEvaluationPipeline
+
+
+
 
 STAGE_NAME = "Data Ingestion Stage"
 try:
@@ -19,6 +25,36 @@ try:
     data_validation = DataValidationPipeline()
     data_validation.main()
     logger.info(f">>>> stage {STAGE_NAME} completed <<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME =  "Data Transformation Stage"
+try:
+    logger.info(f">>>> stage {STAGE_NAME} started <<<<<<")
+    data_transformation = DataTransformationPipeline()
+    data_transformation.main()
+    logger.info(f">>>> stage {STAGE_NAME} completed <<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME =  "Model Training Stage"
+try:
+    logger.info(f">>>> stage {STAGE_NAME} started <<<<<<")
+    model_trainer = ModelTrainerPipeline()
+    model_trainer.main()
+    logger.info(f">>>> stage {STAGE_NAME} completed <<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME =  "Model Evaluation Stage"
+try:
+    logger.info(f">>>> stage {STAGE_NAME} started <<<<<<")
+    model_evaluation = ModelEvaluationPipeline()
+    model_evaluation.main()
+    logger.info(f">>>> stage {STAGE_NAME} completed <<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
     raise e
